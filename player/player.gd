72bcffbc5e6578e18e1564b80eba20d97765e9aa
Paul Methfessel <paul.methfessel@web.de>
2022-01-15 13:09:43 +0100

    Add Player and Movement

--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,27 @@
-extends Node2D
+extends KinematicBody
 
-var color setget set_color
+export var speed = 10
 
-func _network_ready(is_source):
-	if is_source:
-		set_color(Color.from_hsv(rand_range(1, 360), 1, 1))
-		position = Vector2(rand_range(0, 600), rand_range(0, 400))
-	# same value on all clients now!
-	print(color)
+#func _network_ready(is_source):
+#	if is_source:
+#		set_color(Color.from_hsv(rand_range(1, 360), 1, 1))
+#		position = Vector2(rand_range(0, 600), rand_range(0, 400))
+#	# same value on all clients now!
+#	print(color)
 
-func set_color(c: Color):
-	color = c
-	$token.color = c
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_forward"):
+		direction.z += 1
+	if Input.is_action_pressed("move_backward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+	
+	move_and_slide(direction * speed, Vector3.UP)
