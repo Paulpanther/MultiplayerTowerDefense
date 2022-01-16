0b3808ea62da1dc3d99d2aa3cdb4c466c54b3017
Paul Methfessel <paul.methfessel@web.de>
2022-01-16 12:59:58 +0100

    Place Energy Factories with PlacementGhost

--- resources/energy_factory/energy_factory.gd
+++ resources/energy_factory/energy_factory.gd
@@ -1,16 +1,6 @@
 extends Node
 
+var is_ghost = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_ghost(enable):
+	is_ghost = enable
