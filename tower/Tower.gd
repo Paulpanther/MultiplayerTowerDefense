3d21f57ce6f02d36568fd1eb007dd05f7b0a585e
Paul Methfessel <paul.methfessel@web.de>
2022-01-16 02:02:42 +0100

    Add interpolated rotation

--- tower/Tower.gd
+++ tower/Tower.gd
@@ -1,9 +1,11 @@
 extends Spatial
 
-export var shoot_delay = 200
+export var shoot_delay = 700
+export var rotation_speed = 2
 
 var last_shoot = 0
-
+var last_side_left = false
+var following_enemy
 
 func set_ghost(enable):
 	pass
@@ -14,14 +16,25 @@ func _process(delta):
 	if time - last_shoot > shoot_delay:
 		last_shoot = time
 		_shoot()
+	
+	if following_enemy:
+		_interpolate_rotation(delta)
+		#look_at(following_enemy.transform.origin, Vector3.UP)
+
+func _interpolate_rotation(delta):
+	var final = transform.looking_at(following_enemy.transform.origin, Vector3.UP)
+	transform.basis.x = lerp(transform.basis.x, final.basis.x, delta * rotation_speed)
+	transform.basis.y = lerp(transform.basis.y, final.basis.y, delta * rotation_speed)
+	transform.basis.z = lerp(transform.basis.z, final.basis.z, delta * rotation_speed)
 
 func _shoot():
 	var enemy = _get_nearest_enemy()
 	if not enemy:
 		return
+	following_enemy = enemy
 	
-	look_at(enemy.transform.origin, Vector3.UP)
-	$"Nozzle/Particles".restart()
+	_get_current_nozzle().get_node("Particles").restart()
+	last_side_left = not last_side_left
 	
 
 func _get_nearest_enemy():
@@ -35,3 +48,9 @@ func _get_nearest_enemy():
 			min_dist = dist
 			min_enemy = enemy
 	return min_enemy
+
+func _get_current_nozzle():
+	if last_side_left:
+		return $NozzleLeft
+	else:
+		return $NozzleRight
